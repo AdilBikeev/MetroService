9577b4d6c3d3539e295be535232f9f0659aebdde
Adil <bikeev1999@mail.ru>
2019-09-19 08:32:25 +0300

    Правки

---
+++ MetroService/Scripts/ModelDb/CreateMetroDb.sql
@@ -0,0 +1,57 @@
+﻿
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 09/17/2019 23:33:32
+-- Generated from EDMX file: C:\Users\Адиль\Desktop\Обучение в МИРЭА\3 курс\5 семестр\СиП\MetroService\Models\MetroDbModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [MetroDb];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[User]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[User];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'User'
+CREATE TABLE [dbo].[User] (
+    [Id] int  NOT NULL,
+    [login] nvarchar(50)  NOT NULL,
+    [password] nvarchar(50)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'User'
+ALTER TABLE [dbo].[User]
+ADD CONSTRAINT [PK_User]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
---
+++ MetroService/Scripts/ModelDb/InsertData.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO [dbo].[User] ([Id], [login], [password]) VALUES (1, N'1', N'1')
+INSERT INTO [dbo].[User] ([Id], [login], [password]) VALUES (2, N'2', N'2')
+INSERT INTO [dbo].[User] ([Id], [login], [password]) VALUES (3, N'3', N'3')
