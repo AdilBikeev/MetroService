4d47d164352937a4c35f805d30b63eb0188e4419
AdilBikeev <bikeev1999@mail.ru>
2019-11-03 16:10:26 +0300

    service Добавил классы для описания таблиц с данными документов,  с которыми определенные пользователи не ознакомились

---
+++ MetroService/HelperMethods/DocumentHelper.cs
@@ -0,0 +1,27 @@
+﻿using MetroService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MetroService.HelperMethods
+{
+    public static class DocumentHelper
+    {
+        public static string ParceDocument(List<Document> lst)
+        {
+            string listStr = string.Empty;
+
+            for (int i = 0; i < lst.Count; i++)
+            {
+                listStr += lst[i].Name;
+                if(i+1 < lst.Count)
+                {
+                    listStr += ",";
+                }
+            }
+
+            return listStr;
+        }
+    }
+}
